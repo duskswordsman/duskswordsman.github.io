a02350f41cc027cfe8d00912382ecc73dcaeec52
duskswordsman <47051156+duskswordsman@users.noreply.github.com>
2020-09-15 06:35:59 +0300

    Add files via upload

---
+++ MyLittleSelly/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1600140849,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/selenie-sheet0.png",
+		"images/selenie-sheet1.png",
+		"images/coin-sheet0.png",
+		"images/scorepoint-sheet0.png",
+		"images/portal-sheet0.png",
+		"images/tilemap.png",
+		"images/tilebackground.png",
+		"images/tiledanger.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
